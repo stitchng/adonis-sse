b446382a4fe8c4ec910219ed02d62009e9c3eb90
StitchNG <stitchnig@gmail.com>
2019-10-18 13:00:45 +0100

    Update index.js

--- src/Stream/index.js
+++ src/Stream/index.js
@@ -7,9 +7,9 @@ class Stream {
     this.init = EventStream.init;
     this.options = {
       detect_ie_and_pad_with_comments: Config.get('sse.detect_ie')
-      compress_output: Config.get('compress_output'),
-      prefer_event_name: Config.get('prefer_event_name'),
-      prefered_event_name: Config.get('prefered_event_name')
+      compress_output: Config.get('sse.compress_output'),
+      prefer_event_name: Config.get('sse.prefer_event_name'),
+      prefered_event_name: Config.get('sse.prefered_event_name')
     }
   }
 
